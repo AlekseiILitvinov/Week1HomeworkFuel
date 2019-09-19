6bba8403ab1207d59f250f8eb7160f0bc9d87b2b
AlekseiILitvinov <litvinovai988@gmail.com>
2019-09-19 11:13:22 +0300

    Changed consumption to double.

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,16 @@ import java.util.Scanner;
  */
 public class Main {
 	public static void main(String[] args) {
-		int fuelLeft = 33; //default fuel left
-		int usagePerHundred = 15; //default fuel consumption
+		int fuelLeft;
+		double usagePerHundred;
 
-		//Uncomment the lines below for extra interactivity
-//		Scanner scanner = new Scanner(System.in);
-//		System.out.print("Please enter the fuel remaining: ");
-//		fuelLeft = scanner.nextInt();
-//		System.out.print("Please enter your car's fuel consumption (per 100 km.): ");
-//		usagePerHundred = scanner.nextInt();
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Please enter the fuel remaining: ");
+		fuelLeft = scanner.nextInt();
+		System.out.print("Please enter your car's fuel consumption (per 100 km.): ");
+		usagePerHundred = Double.parseDouble(scanner.next());
 
-		int distanceRemaining = fuelLeft * 100 / usagePerHundred;
+		int distanceRemaining = (int) (fuelLeft * 100 / usagePerHundred);
 		System.out.println("With " + fuelLeft + " L of fuel left and " + usagePerHundred + " L used per 100 km Your " +
 				"car can travel approximately another " + distanceRemaining + " km.");
 		if (distanceRemaining < 50) {
